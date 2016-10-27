1f8588c2e36097d7c7e935594c995d44f17c929a
Carlos Ochoa <cgochoa@acid.cl>
2016-10-26 22:31:44 -0300

    add search (landing) and results pages (without style)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   protect_from_forgery with: :exception
 
-  def index
+  def search
+  end
+
+  def results
+    session[:search] = {} unless session[:search].present?
+
+    if params[:search].present?
+      session[:search]['patente'] = params[:search][:patente]
+      session[:search]['kms']     = params[:search][:kms]
+    end
+
+    if session[:search].nil? or session[:search]['patente'].nil? or session[:search]['kms'].nil?
+      redirect_to :search_home
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "home#search"
+
+  get 'index' => 'home#search'
+  get 'search' => 'home#search', as: :search_home
+  match 'results' => 'home#results', as: :results_home, via: [:get, :post]
 end
