eee8acd1ac82186f2d5e13897f2518837fd43968
Rodrigo Alvarez <rodrigo@kolorsheep.com>
2016-11-09 09:50:41 -0300

    add font files to asset papiline

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module MisMantenciones
     config.i18n.locale = config.i18n.default_locale
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
