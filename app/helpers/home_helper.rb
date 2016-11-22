2b3033540261de9d06121a63f3f5f16dcf77f0b2
Carlos Ochoa <cgochoa@acid.cl>
2016-11-21 22:15:21 -0300

    add badge description

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,10 @@ module HomeHelper
   def manteinance_item_status_class(relation)
     "disabled" unless relation.any?
   end
+
+  def manteinance_items_list(manteinance_items)
+    manteinance_items.collect do |manteinance_item|
+      manteinance_item.desc_mantencion
+    end.join(', ')
+  end
 end
