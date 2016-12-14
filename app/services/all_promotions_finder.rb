d52b7ad8d54addfd6ea42f7c2efcd65c894c50b7
Carlos Ochoa <cgochoa@acid.cl>
2016-12-13 22:30:22 -0300

    apply order depending on priority and vme presence

--- app/services/all_promotions_finder.rb
+++ app/services/all_promotions_finder.rb
@@ -3,7 +3,7 @@ class AllPromotionsFinder < BaseService
   def call
     promotions = Promotion.availables.actives
     .includes(:branches, :category, attributes_promotions: :promotion_attribute)
-    .order("promotions.created_at desc")
+    .order("promotions.priority desc, promotions.promo_price asc, promotions.created_at desc")
     if params[:category_id]
       promotions = promotions.where(category_id: params[:category_id])
     end
--- app/services/carousel_products_finder.rb
+++ app/services/carousel_products_finder.rb
@@ -6,7 +6,7 @@ class CarouselProductsFinder < BaseService
   PRODUCTS_TO_SHOW_IN_CAROUSEL = 20
 
   def call
-    carousel_products(get_products, get_category_array)
+    carousel_products(get_products_with_vmes + get_products_without_vmes, get_category_array)
   end
 
   private
@@ -15,16 +15,24 @@ class CarouselProductsFinder < BaseService
     Carousel::Selector.new(size: PRODUCTS_TO_SHOW_IN_CAROUSEL, objects: products, categories: category_circular_array).items
   end
 
-  def get_products
+  def get_products_base
     products = Product.not_deleted
     .includes(:branches, :category)
     .joins(:products_vmes)
     .order("products.created_at desc")
+    .limit(PRODUCTS_TO_SHOW_IN_CAROUSEL)
+  end
+
+  def get_products_with_vmes
+    products = []
     if params[:vehicle].present? and params[:vehicle].vme.present?
-      products = products.where("(products_vmes.vme_id in (?) or products_vmes.vme_id IS NULL)", params[:vehicle].vme.vme_id)
-    else
-      products = products.where("products_vmes.vme_id IS NULL")
+      products = get_products_base.where("(products_vmes.vme_id in (?))", params[:vehicle].vme.vme_id).to_a
     end
+    products
+  end
+
+  def get_products_without_vmes
+    products = get_products_base.where("products_vmes.vme_id IS NULL").to_a
   end
 
   def get_category_array
