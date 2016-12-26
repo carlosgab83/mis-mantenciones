87fd61a4d10a1fd959ca99b7a9559cb572a5479a
Carlos Ochoa <cgochoa@acid.cl>
2016-12-26 14:56:03 -0300

    add new css route to find price on cambiatuneimatico.com

--- lib/product_scraper/cambia_tu_neumatico.rb
+++ lib/product_scraper/cambia_tu_neumatico.rb
@@ -2,7 +2,8 @@ module ProductScraper
   class CambiaTuNeumatico < Base
 
     def price
-      numerize_price page.at('.prod-precio .red').try(:text).try(:strip)
+      numerize_price(page.at('.prod-precio .red').try(:text).try(:strip)) ||
+      numerize_price(page.at('.precio-normal-ficha').try(:text).try(:strip))
     end
   end
 end
