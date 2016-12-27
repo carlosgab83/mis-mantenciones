68c86fe888049d63f6f4a3c9616f216df4bac7c9
Carlos Ochoa <cgochoa@acid.cl>
2016-12-27 00:56:37 -0300

    Product scraping cache is stored in db

--- app/models/branches_product.rb
+++ app/models/branches_product.rb
@@ -7,9 +7,8 @@ class BranchesProduct < ApplicationRecord
   end
 
   def cached_price
-    # return 0 if Rails.env.development?
+    return 0 if Rails.env.development?
     db_minutes = SystemSetting.config.try(:product_scraping_caching_minutes) || 2
-    return nil if url.nil? or url.blank?
     Rails.cache.fetch("#{cache_key}_cached_price_#{branch_id}_#{product_id}", expires_in: db_minutes.minutes) do
       ProductScraper::Base.new.scraper_instance(url).price
     end
