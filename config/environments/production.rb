55f01062a44d2f80766b05e20359720ff9c341fe
Carlos Ochoa <cgochoa@acid.cl>
2017-01-17 20:04:32 -0300

    add mandrill credentials for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mandrillapp.com",
+    :domain         => "mismantenciones.com",
+    :user_name      => ENV['MAILER_USER'],
+    :password       => ENV['MAILER_PASSWORD'],
+    authentication:       :login,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
