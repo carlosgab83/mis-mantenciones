f224744573476f98380fd8bc2d12adecdcf8fec4
Carlos Ochoa <cgochoa@acid.cl>
2017-01-22 01:56:20 -0300

    initialize mandrill manteinante coupon email integration

--- app/mailers/manteinance_coupon_created_mailer.rb
+++ app/mailers/manteinance_coupon_created_mailer.rb
@@ -1,11 +1,27 @@
 class ManteinanceCouponCreatedMailer < ApplicationMailer
 
   def notify_client(manteinance_coupon, vehicle_brand, vehicle_model)
-    @manteinance_coupon = manteinance_coupon
-    @vehicle_brand = vehicle_brand
-    @vehicle_model = vehicle_model
-    mail  to: @manteinance_coupon.client.email,
-          subject: "Has adquirido una #{@manteinance_coupon.pauta.name} para el #{@manteinance_coupon.date} en #{@manteinance_coupon.branch.shop.name}."
+    template_name = 'mismantenciones-com-ticket'
+    template_content = []
+    message = {
+      from_name: default_from_name,
+      from_email: default_from_email,
+      to: [{email: manteinance_coupon.client.email}],
+      subject: "Has adquirido una #{manteinance_coupon.pauta.name} para el #{manteinance_coupon.date} en #{manteinance_coupon.branch.shop.name}.",
+      merge_vars: [
+        {
+          rcpt: manteinance_coupon.client.email,
+          vars: [
+            {name: 'CLIENT_NAME', content: manteinance_coupon.client.name},
+            {name: 'VEHICLE_NAME', content: "#{vehicle_brand} #{vehicle_model}"},
+            {name: 'SHOP_NAME', content: manteinance_coupon.branch.shop.name},
+            {name: 'BRANCH_NAME', content: manteinance_coupon.branch.name},
+            {name: 'TICKET_NUMBER', content: manteinance_coupon.id}
+          ]
+        }
+      ]
+    }
+    mandrill_client.messages.send_template template_name, template_content, message
   end
 
   def notify_branch(manteinance_coupon, vehicle_brand, vehicle_model)
