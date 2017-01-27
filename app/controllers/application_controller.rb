ab7a363657009c5c554a073cd2e4c1f3359239ae
Carlos Ochoa <cgochoa@acid.cl>
2017-01-27 02:57:36 -0300

    add manteinance coupons client and branch emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :reset_session
+
+  before_filter :assign_url_patent
+
+  # For clicking from email
+  def assign_url_patent
+    if params[:patent].present?
+      session[:vehicle] = VehicleFinder.new(SearchVehicleForm.new('patent' => params[:patent], 'kms' => params[:kms])).call
+    end
+  end
 end
