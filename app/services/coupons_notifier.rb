a75bf687a80eeef00e68677c4b9c3e4e2d534e3f
Carlos Ochoa <cgochoa@acid.cl>
2017-01-27 04:48:39 -0300

    add deliver_later to coupon emails

--- app/services/coupons_notifier.rb
+++ app/services/coupons_notifier.rb
@@ -4,7 +4,7 @@ class CouponsNotifier < BaseService
 
   def call
     promotions_ids = CarouselPromotionsFinder.new(vehicle: params[:vehicle]).call[0..(PROMOTIONS_TO_SHOW_IN_EMAIL-1)].map &:id
-    CouponCreatedMailer.notify_client(params[:coupon], promotions_ids, params[:vehicle].patent, params[:vehicle].kms).deliver
+    CouponCreatedMailer.notify_client(params[:coupon], promotions_ids, params[:vehicle].patent, params[:vehicle].kms).deliver_later
     CouponCreatedMailer.notify_shop(params[:coupon], params[:vehicle].patent, params[:vehicle].kms).deliver_later
   end
 end
