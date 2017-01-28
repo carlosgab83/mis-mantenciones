ec8faac17f9efb96872891c906234aa7295b40cf
Carlos Ochoa <cgochoa@acid.cl>
2017-01-28 01:08:43 -0300

    map all errors to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :reset_session
 
-  before_filter :assign_url_patent
+  before_action :assign_url_patent
 
   # For clicking from email
   def assign_url_patent
