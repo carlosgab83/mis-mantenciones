a4e90011882202d0a200e4fdc7016d22a967f1f5
Carlos Ochoa <cgochoa@acid.cl>
2017-05-21 15:45:04 -0400

    remove deprecated branch serializer method

--- app/serializers/branch_serializer.rb
+++ app/serializers/branch_serializer.rb
@@ -1,16 +1,4 @@
 module BranchSerializer
-  def to_manteinance_alternatives_builder(pauta)
-    Jbuilder.new do |json|
-      json.id           id
-      json.name         name
-      json.shop_id      shop_id
-      json.shop_name    shop.name
-      json.will_contact will_contact
-      json.booking_url  booking_url
-      json.branch_pauta branch_pauta(pauta).to_builder
-    end
-  end
-
   def to_event_tracker_builder
     Jbuilder.new do |json|
       json.branch_id           id
