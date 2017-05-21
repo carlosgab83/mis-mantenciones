535ada8d8e950ae68c3a16524a72e83cc23a4289
Carlos Ochoa <cgochoa@acid.cl>
2017-05-21 15:43:35 -0400

    delete deprecated models

--- app/models/branches_manteinance_item.rb
+++
@@ -1,7 +0,0 @@
-class BranchesManteinanceItem < ApplicationRecord
-  belongs_to :pauta
-  belongs_to :manteinance_item
-  belongs_to :branch
-
-  include BranchesManteinanceItemSerializer
-end
--- app/models/manteinance_coupon.rb
+++
@@ -1,9 +0,0 @@
-class ManteinanceCoupon < ApplicationRecord
-  validates :date, :client_id, :branch_id, :price, :pauta_id, presence: true
-
-  belongs_to :pauta
-  belongs_to :client
-  belongs_to :branch
-
-  include ManteinanceCouponSerializer
-end
