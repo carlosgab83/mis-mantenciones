6da1c9c25b8a65b20b039981d12b6028db8c5ff3
Carlos Ochoa <cgochoa@acid.cl>
2017-05-21 16:15:00 -0400

    delete references to deprecated models

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -5,7 +5,6 @@ class Branch < ApplicationRecord
   belongs_to :plan
   has_many :branches_products
   has_many :products, through: :branches_products
-  has_many :branches_manteinance_items
 
   HIDE_BRANCH_PRICE_VALUE = -999
 
@@ -15,36 +14,8 @@ class Branch < ApplicationRecord
     actives.includes(:shop).where(id: ids_for_pauta(pauta))
   end
 
-  scope :ids_for_pauta, -> (pauta) do
-    BranchesManteinanceItem.where(pauta: pauta).pluck(:branch_id).uniq &
-    Branch.actives.pluck(:id)
-  end
-
   scope :actives, -> do
     joins(:shop).where("shops.status is true")
   end
 
-  def branches_manteinance_items_by_pauta(pauta)
-    branches_manteinance_items.includes(manteinance_item: :section_type).where(pauta: pauta)
-  end
-
-  def branches_manteinance_items_count_by_pauta(pauta)
-    branches_manteinance_items.select("count(branches_manteinance_items.id) as count_manteinance_items")
-    .where(pauta: pauta)
-    .order('')
-    .first['count_manteinance_items']
-  end
-
-  # Loads an obj that stores all manteinance_items offered by this branch for this pauta
-  def branch_pauta(pauta)
-    BranchPauta.new(branch: self, pauta: pauta).load
-  end
-
-  def promo_price(pauta)
-    branch_pauta(pauta).promo_price
-  end
-
-  def full_price(pauta)
-    branch_pauta(pauta).full_price
-  end
 end
