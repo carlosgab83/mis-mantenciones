9c8ed29190371ca8da63d4eb03378f40e79d64bd
Carlos Ochoa <cgochoa@acid.cl>
2017-05-23 01:32:46 -0400

    add interval between jumps to branchs serializer

--- app/serializers/branches_serializer.rb
+++ app/serializers/branches_serializer.rb
@@ -2,7 +2,7 @@ module BranchesSerializer
   def to_map_builder(branches)
     Jbuilder.new do |json|
       collection = branches.map do |branch|
-        [branch.id, branch.name, branch.latitude, branch.longitude, branch.branch_type.marker_url]
+        [branch.id, branch.name, branch.latitude, branch.longitude, branch.branch_type.marker_url, branch.interval_between_jumps]
       end
 
       json.array! collection
