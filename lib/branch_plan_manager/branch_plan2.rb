6dfc8da0520e1d39663b2ecc970b4052b9e1c388
Carlos Ochoa <cgochoa@acid.cl>
2017-06-05 00:03:50 -0400

    add basic logic for branch_plan2

--- lib/branch_plan_manager/branch_plan2.rb
+++ lib/branch_plan_manager/branch_plan2.rb
@@ -1,4 +1,15 @@
 module BranchPlanManager
   class BranchPlan2 < Base
+
+    def items
+      branch_information = branch_information_relation
+      promotions = OtherPromotion.availables.actives.with_stock.not_deleted
+        .joins(branches_promotions: :branch)
+        .where("branches.id = ?", branch.id)
+        .order("promotions.priority desc, promotions.promo_price asc, promotions.created_at desc")
+        .limit(PROMOTIONS_LIMIT)
+
+      {promotions: branch_information + promotions, products: branch_products}
+    end
   end
 end
