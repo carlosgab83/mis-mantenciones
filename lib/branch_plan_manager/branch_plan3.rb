efb7c7d4c10f9fb394d172f6aa469cdefb858949
Carlos Ochoa <cgochoa@acid.cl>
2017-06-07 22:22:24 -0400

    find promotions and manteinances based on model and brand id

--- lib/branch_plan_manager/branch_plan3.rb
+++ lib/branch_plan_manager/branch_plan3.rb
@@ -12,44 +12,42 @@ module BranchPlanManager
     private
 
     def promotions
-      promotions = OtherPromotion.availables.actives.with_stock.not_deleted
+      promotions_or_manteinances_by_vehicle(OtherPromotion)
+    end
+
+    def manteinances
+      promotions_or_manteinances_by_vehicle(Manteinance)
+    end
+
+    def promotions_or_manteinances_by_vehicle(promotion_type_class)
+      _result_relation = promotion_type_class.availables.actives.with_stock.not_deleted
         .joins(branches_promotions: :branch)
         .joins(promotions_vmes: {vme: {model: :brand}})
         .where("branches.id = ?", branch.id)
 
       if user_input.brand_id.present?
-        promotions = promotions.where("marca.id_marca = ?", user_input.brand_id)
+        _result_relation = _result_relation.where("marca.id_marca = ?", user_input.brand_id)
       end
 
       if user_input.model_id.present?
-        promotions = promotions.where("modelo.id_modelo = ?", user_input.model_id)
+        _result_relation = _result_relation.where("modelo.id_modelo = ?", user_input.model_id)
       end
 
-      promotions
+      if user_input.patent.present?
+        vehicle = VehicleFinder.new(SearchVehicleForm.new('patent'=> user_input.patent)).call
+
+        if vehicle.vme.present?
+          _result_relation = _result_relation.where("vehiculo_modelo_especifico.vme_id = ?", vehicle.vme.vme_id)
+        end
+
+        if user_input.kms.present?
+          _result_relation = _result_relation.where("promotions.kms IS NOT NULL")
+          _result_relation = _result_relation.order("abs(promotions.kms - #{user_input.kms})")
+        end
+      end
+
+      _result_relation
     end
 
-    def manteinances
-      manteinances = Manteinance.none
-      if not user_input.patent.present? and user_input.kms.present?
-        return manteinances
-      end
-
-      vehicle = VehicleFinder.new(SearchVehicleForm.new('patent'=> user_input.patent)).call
-
-      if vehicle.vme.present?
-        manteinances = Manteinance.availables.actives.with_stock.not_deleted
-          .joins(branches_promotions: :branch)
-          .joins(:vmes)
-          .where("branches.id = ?", branch.id)
-          .where("vehiculo_modelo_especifico.vme_id = ?", vehicle.vme.vme_id)
-      end
-
-      if user_input.kms.present?
-        manteinances = manteinances.where("promotions.kms IS NOT NULL")
-        manteinances = manteinances.order("abs(promotions.kms - #{user_input.kms})")
-      end
-
-      manteinances
-    end
   end
 end
