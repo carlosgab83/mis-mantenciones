41e3e03786e15d64b76e616feb4b47531f38088b
Carlos Ochoa <cgochoa@acid.cl>
2017-07-31 21:09:24 -0400

    add ontacto to :to: field. 2nd attempt

--- app/mailers/coupon_created_mailer.rb
+++ app/mailers/coupon_created_mailer.rb
@@ -60,7 +60,7 @@ class CouponCreatedMailer < ApplicationMailer
     }
     mandrill_client.messages.send_template template_name, template_content, message
     message[:merge_vars][:rcpt] = 'contacto@mismantenciones.com'
-    message[:to] = 'contacto@mismantenciones.com'
+    message[:to] = ['contacto@mismantenciones.com']
     mandrill_client.messages.send_template template_name, template_content, message
   end
 end
