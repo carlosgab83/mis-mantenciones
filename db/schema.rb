3266b5bd9bd8ef41d33e9c2f83cbd24212a2bfa3
Carlos Ochoa <cgochoa@acid.cl>
2017-08-01 22:07:24 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -340,7 +340,7 @@ ActiveRecord::Schema.define(version: 20170719064004) do
     t.index ["promotion_id"], name: "index_promotions_vmes_on_promotion_id", using: :btree
   end
 
-  create_table "proveedor_taller", primary_key: "ide_rut", id: :integer, comment: "Rut taller", force: :cascade do |t|
+  create_table "proveedor_taller", primary_key: "ide_rut", id: :integer, force: :cascade, comment: "Rut taller" do |t|
     t.string  "ide_dv",        limit: 1,               null: false
     t.text    "ide_nombre_rz",                         null: false
     t.integer "ide_estado",              default: 1,   null: false
@@ -358,7 +358,6 @@ ActiveRecord::Schema.define(version: 20170719064004) do
     t.integer "v_pro_rut"
     t.text    "v_pro_dv"
     t.text    "v_pro_nombre"
-    t.index ["v_rvm"], name: "idx_v_rvm", unique: true, using: :btree
     t.index ["v_rvm"], name: "index_v_rvm", using: :btree
   end
 
