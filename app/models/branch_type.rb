1dd99b317a893713da7f2cf03ddb89ae0cb531c9
Carlos Ochoa <cgochoa@acid.cl>
2017-09-23 16:13:49 -0300

    add branch_type serializer

--- app/models/branch_type.rb
+++ app/models/branch_type.rb
@@ -1,4 +1,5 @@
 class BranchType < ApplicationRecord
+  include BranchTypeSerializer
 
   default_scope {where("branch_types.deleted = false or branch_types.deleted IS NULL")}
   scope :actives, -> {where("branch_types.status is true")}
