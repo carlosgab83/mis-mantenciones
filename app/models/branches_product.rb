b1ac1786baef1d12348eeab5dbff5d1a41a1f25f
Carlos Ochoa <cgochoa@acid.cl>
2018-01-01 15:47:21 -0300

    add and modidy models in order to checkout and buy

--- app/models/branches_product.rb
+++ app/models/branches_product.rb
@@ -7,7 +7,8 @@ class BranchesProduct < ApplicationRecord
   end
 
   def cached_price
-    return 0 if Rails.env.development?
+    # return 0 if Rails.env.development?
+    return 250000 if Rails.env.development?
     db_minutes = SystemSetting.config.try(:product_scraping_caching_minutes) || 2
     Rails.cache.fetch("#{cache_key}_cached_price_#{branch_id}_#{product_id}", expires_in: db_minutes.minutes) do
       _price = ProductScraper::Base.new.scraper_instance(url).price
--- app/models/branches_promotion.rb
+++ app/models/branches_promotion.rb
@@ -1,4 +1,8 @@
 class BranchesPromotion < ApplicationRecord
   belongs_to :branch
   belongs_to :promotion
+
+  def price
+    promotion.try(:promo_price)
+  end
 end
