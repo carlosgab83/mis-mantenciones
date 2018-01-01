4cf5b8ed972d69a1d14429dedf10e10044202db2
Carlos Ochoa <cgochoa@acid.cl>
2018-01-01 15:48:05 -0300

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,15 @@ Rails.application.routes.draw do
 
   resources :shop_inscriptions, :path => '/registro-taller', only: [:new, :create, :update]
 
-  get 'pay_test' => 'home#pay_test', as: :pay_test
+  resources :checkouts, :path => '/checkout', only: [:create] do
+    collection do
+      get :update_price_by_quantity
+    end
+  end
+
+  resources :payment_results, :path => '/payment_results', only: [:create]
+
+  resources :orders, only: [:create]
 
   resources :webpay, only:[] do
     collection do
