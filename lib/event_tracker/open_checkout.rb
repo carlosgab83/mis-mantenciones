ac6d1317721b9adb134097fd603acc0849bb681b
Carlos Ochoa <cgochoa@acid.cl>
2018-02-17 15:17:18 -0300

    add event track for open checkout

---
+++ lib/event_tracker/open_checkout.rb
@@ -0,0 +1,16 @@
+module EventTracker
+  class OpenCheckout < Base
+    def initialize(options = {})
+      super(options)
+      self.attrs.merge!(options[:vehicle].to_event_tracker_builder.attributes!)
+      self.attrs.merge!(options[:client].to_event_tracker_builder.attributes!) if options[:client]
+      self.attrs.merge!(options[:product].to_event_tracker_builder.attributes!) if options[:product]
+      self.attrs.merge!(options[:promotion].to_event_tracker_builder.attributes!) if options[:promotion]
+      self.attrs.merge!(options[:branch].to_event_tracker_builder.attributes!)
+    end
+
+    def track
+      super("Open Checkout")
+    end
+  end
+end
