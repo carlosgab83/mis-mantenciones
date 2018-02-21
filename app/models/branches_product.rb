a19bbdee35a24e77f78552d33e481e209b2bc3e8
Carlos Ochoa <cgochoa@acid.cl>
2018-02-21 09:59:47 -0300

    fix infinite loop

--- app/models/branches_product.rb
+++ app/models/branches_product.rb
@@ -15,8 +15,12 @@ class BranchesProduct < ApplicationRecord
     db_minutes = SystemSetting.config.try(:product_scraping_caching_minutes) || 2
     Rails.cache.fetch("#{cache_key}_cached_price_#{branch_id}_#{product_id}", expires_in: db_minutes.minutes) do
       _price = ProductScraper::Base.new.scraper_instance(url).price
-      self.price = _price
-      save
+
+      Thread.new{
+        self.price = _price
+        save
+      }
+
       _price
     end
   end
