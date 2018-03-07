b57c7d59845faa548c4a783b0346afd64553e3c1
Carlos Ochoa <cgochoa@acid.cl>
2018-03-06 22:37:14 -0300

    add branch email and fix price format

--- app/services/success_payment_notifier.rb
+++ app/services/success_payment_notifier.rb
@@ -5,5 +5,6 @@ class SuccessPaymentNotifier < BaseService
   def call
     promotions_ids = CarouselPromotionsFinder.new(vehicle: params[:vehicle]).call[0..(PROMOTIONS_TO_SHOW_IN_EMAIL-1)].map(&:id)
     SuccessPaymentMailer.notify_client(params[:payment], promotions_ids, params[:vehicle].patent, params[:vehicle].kms).deliver_later
+    SuccessPaymentMailer.notify_branch(params[:payment]).deliver_later
   end
 end
