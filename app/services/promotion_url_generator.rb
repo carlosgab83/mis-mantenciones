4823b2467d9f285740c06a49a6f8e68df5aae845
Carlos Ochoa <cgochoa@acid.cl>
2018-03-18 23:19:48 -0300

    fix email carousel url to promotinos

--- app/services/promotion_url_generator.rb
+++ app/services/promotion_url_generator.rb
@@ -9,7 +9,14 @@ class PromotionUrlGenerator < BaseService
   private
 
   def url_generator
-     Rails.application.routes.url_helpers.promotion_url(params[:promotion], patent: params[:patent], kms: params[:kms], host: base_host)
+    Rails.application.routes.url_helpers.shop_branch_promotion_url(
+      params[:promotion].first_shop.slug,
+      params[:promotion].first_branch.slug,
+      params[:promotion].slug,
+      patent: params[:patent],
+      kms: params[:kms],
+      host: base_host
+    )
   end
 
 end
