c061e2171b9d7457cfc80585942607850c3be810
Carlos Ochoa <cgochoa@acid.cl>
2018-03-20 21:45:30 -0300

    add logic for new promotions in checkout behavior

--- lib/event_tracker/open_checkout.rb
+++ lib/event_tracker/open_checkout.rb
@@ -6,7 +6,7 @@ module EventTracker
       self.attrs.merge!(options[:client].to_event_tracker_builder.attributes!) if options[:client]
       self.attrs.merge!(options[:product].to_event_tracker_builder.attributes!) if options[:product]
       self.attrs.merge!(options[:promotion].to_event_tracker_builder.attributes!) if options[:promotion]
-      self.attrs.merge!(options[:branch].to_event_tracker_builder.attributes!)
+      self.attrs.merge!(options[:branch].to_event_tracker_builder.attributes!) if options[:branch]
       self.attrs.merge!(price: options[:price])
     end
 
