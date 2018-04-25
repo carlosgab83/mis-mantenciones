4c5377026136cb9a52029b8acdb756837b1a12d2
Carlos Ochoa <cgochoa@acid.cl>
2018-04-25 20:45:54 -0300

    add validations and rails_admin methods in shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,14 @@ class Shop < ApplicationRecord
 
   # Use friendly id based on name
   friendly_id :name, use: :slugged
-
   has_many :branches
+
+  validates :installation_enabled, :click_n_collect_enabled, :delivery_enabled, inclusion: { in: [true, false] }
+
+  # Read automatically by rails_admin
+  %w(installation_enabled click_n_collect_enabled delivery_enabled).each do |method_name|
+    define_method :"#{method_name}_enum" do
+        [['Si', true], ['No', false]]
+    end
+  end
 end
