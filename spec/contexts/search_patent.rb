a7bbcf5c8157c11df038f04153f88932c96d8196
Carlos Ochoa <carlosgab83@gmail.com>
2018-05-23 19:28:58 -0400

    fix specs

--- spec/contexts/search_patent.rb
+++ spec/contexts/search_patent.rb
@@ -5,14 +5,15 @@
 require 'contexts/shared.rb'
 
 def create_search_patent_context
-    create_system_setting_values
-    create_rvms
-    create_brands
-    create_models
-    create_vehicle_type
-    create_vmes
-    create_generic_pautas
-    create_specific_pautas
+  create_system_setting_values
+  create_rvms
+  create_brands
+  create_models
+  create_vehicle_type
+  create_vmes
+  create_generic_pautas
+  create_specific_pautas
+  create_categories
 end
 
 def create_stubs
