bccbe0e474d5ad3a3e74ebd6047f96360120df59
Carlos Ochoa <carlosgab83@gmail.com>
2018-05-23 20:36:51 -0400

    add scopes to home_banner model

--- app/models/home_banner.rb
+++ app/models/home_banner.rb
@@ -1,5 +1,8 @@
 class HomeBanner < ApplicationRecord
   validates :name, :title, :subtitle, :image_url, :image_click_url, :button_text,
-    :button_click_url, :image_click_new_page, :button_click_new_page,
-    :from_date, :to_date, :priority, :status, presence: true
+    :button_click_url, :from_date, :to_date, :priority, presence: true
+
+  scope :availables, -> {where("? between from_date and to_date", Date.today)}
+  scope :actives, -> {where("status is true")}
+  scope :not_deleted, -> {where(deleted: [false, nil])}
 end
