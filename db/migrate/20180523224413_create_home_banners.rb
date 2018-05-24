fd92674375e0f0a289554729a78ad05de8d06312
Carlos Ochoa <carlosgab83@gmail.com>
2018-05-23 20:29:26 -0400

    remigrate table

--- db/migrate/20180523224413_create_home_banners.rb
+++ db/migrate/20180523224413_create_home_banners.rb
@@ -8,8 +8,8 @@ class CreateHomeBanners < ActiveRecord::Migration[5.0]
       t.string  :image_click_url, null: false
       t.string  :button_text, null: false
       t.string  :button_click_url, null: false
-      t.boolean :image_click_new_page, null: false, default: false
-      t.boolean :button_click_new_page, null: false, default: false
+      t.boolean :image_click_new_page, default: false
+      t.boolean :button_click_new_page, default: false
       t.date    :from_date, null: false
       t.date    :to_date, null: false
       t.integer :priority, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -224,8 +224,8 @@ ActiveRecord::Schema.define(version: 20180523224413) do
     t.string   "image_click_url",                       null: false
     t.string   "button_text",                           null: false
     t.string   "button_click_url",                      null: false
-    t.boolean  "image_click_new_page",  default: false, null: false
-    t.boolean  "button_click_new_page", default: false, null: false
+    t.boolean  "image_click_new_page",  default: false
+    t.boolean  "button_click_new_page", default: false
     t.date     "from_date",                             null: false
     t.date     "to_date",                               null: false
     t.integer  "priority",                              null: false
